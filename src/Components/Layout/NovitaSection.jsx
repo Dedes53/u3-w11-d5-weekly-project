631a218a8dba97b4eea4254e2392468dcd2b0dc9
Dedes <federico.lepore73@gmail.com>
2026-02-20 12:34:55 +0100

    radio station card props

--- src/Components/Layout/NovitaSection.jsx
+++ src/Components/Layout/NovitaSection.jsx
@@ -1,14 +1,22 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import RadioStationCard from "./RadioStationCard";
-
+import radioImage1 from '../../assets/images/1a.png'
+import radioImage2 from '../../assets/images/1b.png'
 function NovitaSection() {
     return (
         <Container>
             <h1 className="b-b py-2 mt-3">Novità</h1>
-            <div className="radio-station d-flex align-items-center justify-content-between p-3 mb-3">
-                <RadioStationCard />
-                <RadioStationCard />
-            </div>
+            <Container className="radio-station p-3 mb-3 overflow-hidden">
+                <Row>
+
+                    <RadioStationCard
+                        radioImage={radioImage1}
+                        description="Rilassati, al resto pensiamo noi. Ascolta Apple Music Chill" />
+                    <RadioStationCard
+                        radioImage={radioImage2}
+                        description="Ecco la nuova casa della musica latina" />
+                </Row>
+            </Container>
 
         </Container>
 
--- src/Components/Layout/RadioStationCard.jsx
+++ src/Components/Layout/RadioStationCard.jsx
@@ -1,12 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
 
-
-function RadioStationCard() {
+function RadioStationCard(props) {
     return (
-        <div>
-            <div>
-                <img src="../../assets/images/1a.png" alt="" />
+        <Col xs={8} md={6}>
+            <div className="d-flex flex-column align-items-start justify-content-center">
+                <div>
+                    <p>Nuova stazione radio</p>
+                    <p>{props.description}</p>
+                </div>
+                <img
+                    className="w-100"
+                    src={props.radioImage}
+                    alt="Stazione radio"
+                />
             </div>
-        </div>
+        </Col>
+
     )
 }
 
